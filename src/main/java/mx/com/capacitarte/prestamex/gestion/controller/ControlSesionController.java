753cf4123119ed0d94d87649d5edb08146d7cf8c
Alberto Rojas Crispin <arojacri@emeal.nttdata.com>
2025-06-28 10:54:59 -0600

    Se dockeriza el MS

--- src/main/java/mx/com/capacitarte/prestamex/gestion/controller/ControlSesionController.java
+++ src/main/java/mx/com/capacitarte/prestamex/gestion/controller/ControlSesionController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,6 +58,23 @@ public class ControlSesionController {
 				
 		log.info("OutPut: {}", successResponseEntity);
 		return loginBeanResponse == null ? ResponseEntity.noContent().build() :successResponseEntity;
+	}
+	
+	@GetMapping("/test")
+	public ResponseEntity<?> test() throws Exception{
+		
+		LoginBeanResponse loginBeanResponse = LoginBeanResponse.builder()
+					.idUsuario("12")
+					.usuario("FT192030")
+					.nombre("Feliciano")
+					.apPaterno("Crispin")
+					.apMaterno("Gutierrez")
+					.genero("Masculino")
+					.perfil("admin")
+					.estatusLogin(true)
+					.build();
+			
+		return ResponseEntity.ok().body(loginBeanResponse);
 	}
 	
 }
